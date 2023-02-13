e6e7c95a1e199098018a15b076417ccf419c1d3c
fahadsarki <fahads.mukhtar@gmail.com>
2023-02-13 10:44:57 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import { Footer, Header, Navbar } from './Components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <header className="App-header">
+      <h1>Jamub VTC Solutions Inc</h1>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+export {default as Footer} from "./Footer/footer"
+export {default as Navbar} from "./Navbar/navbar"
+export {default as Header} from "./Header/header"
+// export {default as Comp1} from './Comp1.jsx';
